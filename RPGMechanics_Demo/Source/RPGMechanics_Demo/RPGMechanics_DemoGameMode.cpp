0848856f61a3c7b7fc108e2122c1d6edadcc871c
mratti.k@hotmail.fi <39917514+Bodyburger@users.noreply.github.com>
2022-11-10 19:18:41 +0200

    TryingToFix GameMode Defaults
    https://forums.unrealengine.com/t/setting-default-pawn-in-c/90489

--- RPGMechanics_Demo/Source/RPGMechanics_Demo/RPGMechanics_DemoGameMode.cpp
+++ RPGMechanics_Demo/Source/RPGMechanics_Demo/RPGMechanics_DemoGameMode.cpp
@@ -3,20 +3,34 @@
 #include "RPGMechanics_DemoGameMode.h"
 #include "RPGMechanics_DemoCharacter.h"
 #include "UObject/ConstructorHelpers.h"
+#include "RPGCameraPawnBase.h"
+#include "RPGController.h"
 
 ARPGMechanics_DemoGameMode::ARPGMechanics_DemoGameMode()
 {
-	// set default pawn class to our RPGCameraPawnBase
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClass(TEXT("D:/Unreal Projects/RPGMechanics_Demo_Repo/RPGMechanics_Demo/Source/RPGMechanics_Demo/RPGCameraPawnBase.h"));
-	if (PlayerPawnClass.Class != NULL)
+	if (ARPGCameraPawnBase::StaticClass() != NULL)
 	{
-		DefaultPawnClass = PlayerPawnClass.Class;
+		DefaultPawnClass = ARPGCameraPawnBase::StaticClass();
+	}
+	if (ARPGController::StaticClass() != NULL)
+	{
+		PlayerControllerClass = ARPGController::StaticClass();
 	}
 
-	// set default controller to our RPGController
-	static ConstructorHelpers::FClassFinder<APlayerController> RPGPlayerControllerClass(TEXT("D:/Unreal Projects/RPGMechanics_Demo_Repo/RPGMechanics_Demo/Source/RPGMechanics_Demo/RPGController.h"));
-	if (RPGPlayerControllerClass.Class != NULL)
-	{
-		PlayerControllerClass = RPGPlayerControllerClass.Class;
-	}
+
+
+
+	// set default pawn class to our RPGCameraPawnBase
+	// static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClass(TEXT("D:/Unreal Projects/RPGMechanics_Demo_Repo/RPGMechanics_Demo/Source/RPGMechanics_Demo/RPGCameraPawnBase.h"));
+	// if (PlayerPawnClass.Class != NULL)
+	// {
+	// 	DefaultPawnClass = PlayerPawnClass.Class;
+	// }
+
+	// // set default controller to our RPGController
+	// static ConstructorHelpers::FClassFinder<APlayerController> RPGPlayerControllerClass(TEXT("D:/Unreal Projects/RPGMechanics_Demo_Repo/RPGMechanics_Demo/Source/RPGMechanics_Demo/RPGController.h"));
+	// if (RPGPlayerControllerClass.Class != NULL)
+	// {
+	// 	PlayerControllerClass = RPGPlayerControllerClass.Class;
+	// }
 }
