f1e7b1754e5c82dc546cc374cf1c0b7880e5d3ad
mratti.k@hotmail.fi <39917514+Bodyburger@users.noreply.github.com>
2023-06-08 11:50:29 +0300

    Repo clean up!
    Moving things away first to ignore them in the next commit

--- RPGMechanics_Demo/Source/RPGMechanics_Demo/RPGMechanics_Demo.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RPGMechanics_Demo.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RPGMechanics_Demo, "RPGMechanics_Demo" );
- 
